f1cf0c5221a3e966c725f68a48fdb9e823f2db81
CMW <900930@gmail.com>
2014-04-09 03:00:41 +0200

    Empty commit

--- drawHeatmap.py
+++ drawHeatmap.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     print(" ************************************************* \n\n")
     print(" Je suis en train de travailler... \n\n")
     #while True :
-    main(srcUrl="D:/DEV/GitHub/Sokaris_View3D/Sokaris_View3D/IntersectionsHN.ini", clkMap="hitHN.png", heatMap="heatHN.png", width=613, height=300)
-    main(srcUrl="D:/DEV/GitHub/Sokaris_View3D/Sokaris_View3D/IntersectionsHW.ini", clkMap="hitHW.png", heatMap="heatHW.png", width=710, height=300)
-    main(srcUrl="D:/DEV/GitHub/Sokaris_View3D/Sokaris_View3D/IntersectionsHS.ini", clkMap="hitHS.png", heatMap="heatHS.png", width=613, height=300)
-    main(srcUrl="D:/DEV/GitHub/Sokaris_View3D/Sokaris_View3D/IntersectionsHE.ini", clkMap="hitHE.png", heatMap="heatHE.png", width=710, height=300)
+    main(srcUrl="D:/DEV/GitHub/Sokaris_View3D/Debug/IntersectionsHN.ini", clkMap="hitHN.png", heatMap="D:/DEV/GitHub/Sokaris_View3D/Debug/texture/heatHN.png", width=613, height=300)
+    main(srcUrl="D:/DEV/GitHub/Sokaris_View3D/Debug/IntersectionsHW.ini", clkMap="hitHW.png", heatMap="D:/DEV/GitHub/Sokaris_View3D/Debug/texture/heatHW.png", width=790, height=300)
+    main(srcUrl="D:/DEV/GitHub/Sokaris_View3D/Debug/IntersectionsHS.ini", clkMap="hitHS.png", heatMap="D:/DEV/GitHub/Sokaris_View3D/Debug/texture/heatHS.png", width=613, height=300)
+    main(srcUrl="D:/DEV/GitHub/Sokaris_View3D/Debug/IntersectionsHE.ini", clkMap="hitHE.png", heatMap="D:/DEV/GitHub/Sokaris_View3D/Debug/texture/heatHE.png", width=790, height=300)
